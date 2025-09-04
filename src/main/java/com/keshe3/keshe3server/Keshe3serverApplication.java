0bfc96f391a37ac9cfb69bbc86919ec4ed3470d5
X ZHAO_O <X ZHAO_O@LAPTOP-0I6EQQGD>
2025-09-04 21:39:47 +0800

    250904-fix:mapperscan

--- src/main/java/com/keshe3/keshe3server/Keshe3serverApplication.java
+++ src/main/java/com/keshe3/keshe3server/Keshe3serverApplication.java
@@ -1,9 +1,15 @@
 package com.keshe3.keshe3server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = { "com.keshe3" })
+@MapperScan(basePackages = { "com.keshe3.**.mapper" })
 public class Keshe3serverApplication {
 
     public static void main(String[] args) {
